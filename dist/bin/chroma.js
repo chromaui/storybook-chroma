cff2f02eef302a239a8c3f0d003cd58088034744
Chroma Machine <machine@hichroma.com>
2019-07-09 00:59:51 +0000

    Commit 0dd8c5546dd476518a02c23517c8cdc1d81d645d

--- dist/bin/chroma.js
+++ dist/bin/chroma.js
@@ -5,7 +5,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 
 var _commander = _interopRequireDefault(require("commander"));
 
-_commander.default.version(require('../../package.json').version).command('publish', 'publish a storybook to Chroma', {
+_commander.default.version(require('../../package.json').version).command('publish', 'publish a Storybook to Chroma', {
   isDefault: true
 }).parse(process.argv);
 //# sourceMappingURL=chroma.js.map
